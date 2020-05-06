cd4787c089a866f9bf448c34c460a43e48e0ff8e
Sohil Ladhani <supertakumi86@gmail.com>
2020-05-06 17:49:57 +0530

    Add necessary files

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/* Portfolio text */
+var portFolioText = '<span id="a">root@sohilladhani</span>:<span id="b">~</span><span id="c">#</span> cd CodeNerve/<br>' +
+    'Hey There! This is the portflio website in the skin of a terminal. <br>' +
+    'You can see that, this is how your portfolio would look like.';
+
+$('#console').t({
+    speed: 50
+});
